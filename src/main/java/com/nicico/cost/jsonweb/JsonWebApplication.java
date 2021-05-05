f0f90427dbea6392c5fc34c9dad96136693b3abf
nima <nmoosavi@live.com>
2021-05-05 13:36:40 +0430

    start

---
+++ src/main/java/com/nicico/cost/jsonweb/JsonWebApplication.java
@@ -0,0 +1,13 @@
+package com.nicico.cost.jsonweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nicico/cost/jsonweb/JsonWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicico.cost.jsonweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
